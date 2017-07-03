19fbd0f8b5dea086ee93d02a94d9f6a3dd1f4d74
alari <name.alari@gmail.com>
2017-07-03 18:41:03 +0300

    better drafts list in a bot

--- src/main/scala/mono/bot/script/FetchDraftsScript.scala
+++ src/main/scala/mono/bot/script/FetchDraftsScript.scala
@@ -2,43 +2,55 @@ package mono.bot.script
 
 import mono.core.article.ArticleOps
 import mono.bot.BotScript.{ Op, Scenario }
-import mono.bot.{ BotOps, BotScript, Command }
+import mono.bot._
 import ArticleScript.showArticleContext
+import cats.free.Free
 import mono.core.person.PersonOps
 
 class FetchDraftsScript(implicit
   B: BotOps[BotScript.Op],
                         P: PersonOps[BotScript.Op],
                         A: ArticleOps[BotScript.Op]) extends Script {
-  private val draftR = s"draft([0-9]+)".r
+  private val draftR = s"show:draft:([0-9]+)".r
+  private val listDraftsR = s"show:drafts:([0-9]+)".r
+
+  private val DefaultLimit = 5
+
+  private def showDrafts(state: BotState, offset: Int, limit: Int, m: Incoming.Meta, isReply: Boolean = false): Free[BotScript.Op, BotState] =
+    for {
+      person ← P.findByTelegramId(m.chat.id)
+      drafts ← A.fetchDrafts(person.map(_.id).get, offset, limit)
+      buttons = drafts.values.map { a ⇒
+        Seq(Inline.CallbackButton(a.title, s"show:draft:${a.id}"))
+      } :+ Seq(
+        Option(offset - limit).filter(_ >= 0).map(o ⇒ Inline.CallbackButton("←", s"show:drafts:$o")),
+        Option(offset + limit).filter(_ < drafts.count).map(o ⇒ Inline.CallbackButton("→", s"show:drafts:$o"))
+      ).flatten
+      _ ← if (buttons.size > 1) B.inline(s"Ваши черновики. Всего: ${drafts.count}. Создать новый: /new", buttons, m.chat.id, Some(m.messageId).filter(_ ⇒ isReply))
+      else B.reply("Нет черновиков. Создайте с /new", m)
+    } yield state
 
   override val scenario: Scenario = {
     case (state, Command("drafts", _, m)) ⇒
-      for {
-        // TODO: fail if user not found
-        person ← P.findByTelegramId(m.chat.id)
-        drafts ← A.fetchDrafts(person.map(_.id).get, 0, 100)
+      showDrafts(state, 0, DefaultLimit, m)
 
-        reply = if (drafts.values.isEmpty) "Нет черновиков. Создайте с /new"
-        else drafts.values.map(a ⇒ s"/draft${a.id} **${a.title}**").mkString("\n")
-
-        _ ← B.reply(reply, m)
-      } yield state
-
-    case (state, Command(draftR(id), _, m)) ⇒
+    case (state, InlineCallback(draftR(id), callbackId, m)) ⇒
       // TODO: fail if user not found
-      P.findByTelegramId(m.chat.id).flatMap { person ⇒
-        A.getById(id.toInt)
-          .flatMap {
-            case a if a.authorIds.toList.exists(person.map(_.id).contains) ⇒
-              showArticleContext(a, m)
+      for {
+        person ← P.findByTelegramId(m.chat.id)
+        a ← A.getById(id.toInt)
+        s ← if (a.authorIds.toList.exists(person.map(_.id).contains)) showArticleContext(a, m)
+        else for {
+          _ ← B.say("Черновик не найден", m)
+        } yield state
+        _ ← B.answer(None, callbackId)
+      } yield s
 
-            case _ ⇒
-              for {
-                _ ← B.say("Черновик не найден", m)
-              } yield state
-          }
-      }
+    case (state, InlineCallback(listDraftsR(offset), callbackId, m)) ⇒
+      for {
+        _ ← showDrafts(state, offset.toInt, DefaultLimit, m, isReply = true)
+        _ ← B.answer(None, callbackId)
+      } yield state
   }
 }
 
