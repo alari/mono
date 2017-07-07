5070c5c47c53750a6af346a0fb1f6b447228a7ca
alari <name.alari@gmail.com>
2017-07-07 11:48:10 +0300

    apply default formatting

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Page from '../components/Page.js'
 import Link from 'next/link'
 
-import { graphql, withApollo, compose } from 'react-apollo'
+import {graphql, withApollo, compose} from 'react-apollo'
 import gql from 'graphql-tag'
 
 import withData from '../lib/with-data'
 
-const Index = ({data:{articles:{count,values}}}) => {
+const Index = ({data: {articles: {count, values}}}) => {
     return (
         <Page title="Mono(noke) Index">
             <h1>Articles: {count}</h1>
             {values.map(article => (<article id={article.id}>
-                <Link to={'/article?id='+article.id} as={article.alias && `/${article.alias}`}><h1>{article.title}</h1></Link>
+                <Link to={'/article?id=' + article.id} as={article.alias && `/${article.alias}`}>
+                    <h1>{article.title}</h1></Link>
                 <div>
                     {article.authors.map(person => (<Link to={`/person?id=${person.id}`}><a>{person.name}</a></Link>))}
                 </div>
